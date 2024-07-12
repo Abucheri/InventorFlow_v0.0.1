85e7ed73b9f47b64083702b8d620aeb23d6eb6b3
O'Brien <abucheriobrien@gmail.com>
2024-07-12 11:53:54 +0300

    added logic for units

--- app.py
+++ app.py
@@ -122,45 +122,84 @@ def delete_category(id):
     flash(f'Category "{category_name}" deleted successfully!', 'success')
     return redirect(url_for('categories'))
 
-
-@app.route('/suppliers', methods=['GET', 'POST'], strict_slashes=False)
-def suppliers():
-    """
-    Suppliers management page.
-    """
-    return render_template('suppliers.html')
-
-
-@app.route('/products', methods=['GET'], strict_slashes=False)
-def products():
-    """
-    Products management page
-    """
-    return render_template('index.html')
-
-
+# =======units=======
 @app.route('/units', methods=['GET'], strict_slashes=False)
 def units():
-    """
-    units management page.
-    """
-    return render_template('units.html')
+    """Retrieve Units of measurement"""
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    search_query = request.args.get('search', '')
 
+    cursor = mysql.connection.cursor()
 
-@app.route('/transactions', methods=['GET', 'POST'], strict_slashes=False)
-def transactions():
-    """
-    Transactions Management page.
-    """
-    return render_template('transactions.html')
+    if search_query:
+        search_sql = "SELECT * FROM units WHERE unit LIKE %s ORDER BY unit ASC"
+        cursor.execute(search_sql, ('%' + search_query + '%',))
+    else:
+        search_sql = "SELECT * FROM units ORDER BY unit ASC"
+        cursor.execute(search_sql)
 
+    units = cursor.fetchall()
+    cursor.close()
 
-@app.route('/summary', methods=['GET'], strict_slashes=False)
-def summary():
-    """
-    Reports and Analysis page.
-    """
-    return render_template('summary.html')
+    # Paginate the results manually
+    total = len(units)
+    units = units[(page - 1) * per_page:page * per_page]
+    pagination = Pagination(page=page, per_page=per_page, total=total, items=units)
+
+    return render_template('units.html', units=pagination)
+
+@app.route('/add_units', methods=['POST'], strict_slashes=False)
+def add_unit():
+    """Add units to database"""
+    if request.method == 'POST':
+        unit_name = request.form['unit_name']
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO units (id, unit) VALUES (%s, %s)", (str(uuid4()), unit_name))
+        mysql.connection.commit()
+        cursor.close()
+        flash(f'Unit "{unit_name}" added successfully!', 'success')
+        return redirect(url_for('units'))
+
+@app.route('/edit_unit/<id>', methods=['GET', 'POST'], strict_slashes=False)
+def edit_unit(id):
+    """Update units by ID"""
+    if request.method == 'POST':
+        unit_name = request.form['unit_name']
+        cursor = mysql.connection.cursor()
+        cursor.execute("UPDATE units SET unit = %s, updated_at = %s WHERE id = %s", (unit_name, updated_date(), id))
+        mysql.connection.commit()
+        cursor.close()
+        flash(f'Unit "{unit_name}" updated successfully!', 'success')
+        return redirect(url_for('units'))
+    else:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM units WHERE id = %s", (id,))
+        unit = cursor.fetchone()
+        cursor.close()
+        return render_template('edit_unit.html', unit=unit)
+
+@app.route('/delete_unit/<id>', methods=['GET'], strict_slashes=False)
+def delete_unit(id):
+    """Delete units by ID"""
+    cursor = mysql.connection.cursor()
+
+    # First, check if there are any products using this Unit
+    cursor.execute("SELECT * FROM products WHERE product_unit_id = %s", (id,))
+    products_using_unit = cursor.fetchall()
+
+    if products_using_unit:
+        # If there are products, you might choose to delete or update them
+        # For example, delete all products using this category
+        cursor.execute("DELETE FROM products WHERE product_unit_id = %s", (id,))
+        mysql.connection.commit()
+    cursor.execute("SELECT unit FROM units WHERE id = %s", (id,))
+    unit_name = cursor.fetchone()[0]
+    cursor.execute("DELETE FROM units WHERE id = %s", (id,))
+    mysql.connection.commit()
+    cursor.close()
+    flash(f'Unit "{unit_name}" deleted successfully!', 'success')
+    return redirect(url_for('units'))
 
 
 if __name__ == '__main__':
